7a4ac412593b260706f4ab364f82ff3bab1f90c3
toby <contact@bundui.com>
2025-07-25 01:10:45 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <span className="text-3xl font-mono">
+          EcommerceKit
+      </span>
+    </div>
+  );
+}
